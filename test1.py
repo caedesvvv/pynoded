b4efcfc0312ac9bbc21aa8a7db30c77b1833ac9e
Pablo Martin <caedes@delirium.(none)>
2008-03-20 23:08:47 +0100

    refactored cairo code into CairoGraph class.

--- test1.py
+++ test1.py
@@ -25,7 +25,7 @@ class Arrow(object):
         self.y0=y0
         self.y1=y1
         self.color=color
-	def Draw(self,ctx):
+    def Draw(self,ctx):
         ctx.set_line_width(1)
         linewidth,_ = ctx.device_to_user_distance(1.,1.)
         ctx.set_line_width(linewidth)
@@ -58,9 +58,42 @@ class Square(object):
         ctx.rectangle(self.x,self.y,self.w,self.h)
         ctx.stroke()
 
-class GtkBackend(gtk.DrawingArea):
+class CairoGraph(object):
+    def __init__(self):
+        self.objects = []
+        self.scale = (1.0,1.0)
+        self.pos = (0.0,0.0)
+    def Create(self,x,y):
+        obj_size = 10/self.scale[0]
+        self.objects.append(Square(x-(obj_size/2),y-(obj_size/2),obj_size,obj_size))
+        return True
+    def Screen2Surface(self,x,y):
+        x = float(x)
+        y = float(y)
+        return [(x/self.scale[0])-self.pos[0],(y/self.scale[1])-self.pos[1]]
+    def DrawCairo(self,ctx):
+        # set the background
+        ctx.set_source_rgb(0.7,0.7,0.7)
+        ctx.set_operator (cairo.OPERATOR_SOURCE)
+        ctx.paint()
+
+        ctx.scale(*self.scale)
+        ctx.translate (*self.pos)
+
+        #ctx.translate ((width - size) / 2, (height - size) / 2)
+        #ctx.scale(size / 150.0, size / 160.0)
+        for obj in self.objects:
+            obj.Draw(ctx)
+    def Zoom(self,x,y,factor):
+        pre_pos = self.Screen2Surface(x,y)
+        self.scale = map(lambda s: s*factor,self.scale)
+        post_pos = self.Screen2Surface(x,y)
+        self.pos = map(lambda i: self.pos[i]+post_pos[i]-pre_pos[i],range(2))
+
+class GtkBackend(gtk.DrawingArea,CairoGraph):
     def __init__(self):
         super(GtkBackend,self).__init__()
+        CairoGraph.__init__(self)
         self.add_events(gtk.gdk.KEY_PRESS_MASK |
                         gtk.gdk.POINTER_MOTION_MASK |
                         gtk.gdk.BUTTON_PRESS_MASK |
@@ -72,14 +105,11 @@ class GtkBackend(gtk.DrawingArea):
         self.connect('motion_notify_event', self.motion_event)
         self.connect('expose_event', self.expose_event)
         self.connect('scroll_event', self.scroll_event)
-        self.objects = []
-        self.scale = (1.0,1.0)
         self.keyp_cbs = {"+" : lambda ev : Zoom(ev.x,ev.y,1/0.99),
                          "-" : lambda ev : Zoom(ev.x,ev.y,0.99)}
         self.keyr_cbs = {}
         self.mousep_cbs = [self.Create,None,self.StartMove]
         self.mouser_cbs = [None,None,self.EndMove]
-        self.pos = (0.0,0.0)
 
     def key_press_event(self, widget, ev):
         print "KEY EVENT"
@@ -102,21 +132,12 @@ class GtkBackend(gtk.DrawingArea):
         self.pos = [self.pos[0]+x,self.pos[1]+y]
         self.movepos = self.Screen2Surface(ev.x,ev.y)
         self.queue_draw_area(0,0,1000,1000)
-    def Create(self,x,y):
-        obj_size = 10/self.scale[0]
-        self.objects.append(Square(x-(obj_size/2),y-(obj_size/2),obj_size,obj_size))
-        return True
+
     def Redraw(self):
         self.queue_draw_area(0,0,1000,1000)
-    def Screen2Surface(self,x,y):
-        x = float(x)
-        y = float(y)
-        return [(x/self.scale[0])-self.pos[0],(y/self.scale[1])-self.pos[1]]
+
     def Zoom(self,x,y,factor):
-        pre_pos = self.Screen2Surface(x,y)
-        self.scale = map(lambda s: s*factor,self.scale)
-        post_pos = self.Screen2Surface(x,y)
-        self.pos = map(lambda i: self.pos[i]+post_pos[i]-pre_pos[i],range(2))
+        CairoGraph.Zoom(self,x,y,factor)
         self.Redraw()
 
     def scroll_event(self, widget, ev):
@@ -124,6 +145,7 @@ class GtkBackend(gtk.DrawingArea):
             self.Zoom(ev.x,ev.y,0.99)
         else:
             self.Zoom(ev.x,ev.y,1/0.99)
+
     def button_press_event(self, widget, ev):
         x,y = self.Screen2Surface(ev.x,ev.y)
         callback = self.mousep_cbs[ev.button-1]
@@ -135,8 +157,10 @@ class GtkBackend(gtk.DrawingArea):
         callback = self.mouser_cbs[ev.button-1]
         if callback and callback(x,y):
             self.Redraw()
+
     def motion_event(self, widget, ev):
         pass
+
     def expose_event(self, widget, event):
         _, _, width, height = widget.allocation
 
@@ -147,24 +171,13 @@ class GtkBackend(gtk.DrawingArea):
 
         pixmap = gtk.gdk.Pixmap (widget.window, width, height)
         ctx = pixmap.cairo_create()
-        
-        # set the background
-        ctx.set_source_rgb(0.7,0.7,0.7)
-        ctx.set_operator (cairo.OPERATOR_SOURCE)
-        ctx.paint()
 
-        ctx.scale(*self.scale)
-        ctx.translate (*self.pos)
-
-        #ctx.translate ((width - size) / 2, (height - size) / 2)
-        #ctx.scale(size / 150.0, size / 160.0)
-        for obj in self.objects:
-            obj.Draw(ctx)
+        self.DrawCairo(ctx)
 
         # draw on window
         gc = gtk.gdk.GC(widget.window)
         widget.window.draw_drawable(gc, pixmap, 0,0, 0,0, -1,-1)
-        
+
 def run(Widget,title="test app"):
     window = gtk.Window()
     window.set_title(title)
