67988ba2a21e67af72fad91d23413658ae24a1d0
Pablo Martin <caedes@delirium.(none)>
2008-03-20 19:00:08 +0100

    refactored app code into a class.

--- test1.py
+++ test1.py
@@ -30,23 +30,8 @@ from random import randint
 import cairo
 import gtk
 
-objects = []
 
 #print "The secret word is " + word_chosen
-
-def key_event(widget, ev):
-    print "key event"
-
-def button_press_event(widget, ev):
-    print "button press event",ev.x,ev.y,ev.button
-    objects.append(Square(ev.x-5,ev.y-5,10,10))
-    win.queue_draw_area(0,0,1000,1000)
-
-def button_release_event(widget, ev):
-    print "button release event",ev.x,ev.y,ev.button
-
-def motion_event(widget, ev):
-    print "motion event",ev.x,ev.y
 
 class Square(object):
     def __init__(self,x,y,w,h,col=(0.1,0.1,0.1)):
@@ -63,52 +48,66 @@ class Square(object):
         ctx.rectangle(self.x,self.y,self.w,self.h)
         ctx.stroke()
 
-def expose_event(widget, event):
-    _, _, width, height = widget.allocation
+class GtkBackend(gtk.DrawingArea):
+    def __init__(self):
+        super(GtkBackend,self).__init__()
+        self.add_events(gtk.gdk.KEY_PRESS_MASK |
+                     gtk.gdk.POINTER_MOTION_MASK |
+                     gtk.gdk.BUTTON_PRESS_MASK |
+                     gtk.gdk.BUTTON_RELEASE_MASK |
+                     gtk.gdk.SCROLL_MASK)
+        self.connect('key_press_event', self.key_event)
+        self.connect('button_press_event', self.button_press_event)
+        self.connect('button_release_event', self.button_release_event)
+        self.connect('motion_notify_event', self.motion_event)
+        self.connect('expose_event', self.expose_event)
+        self.objects = []
+    def key_event(self, widget, ev):
+        print "key event"
+    def button_press_event(self, widget, ev):
+        print "button press event",ev.x,ev.y,ev.button
+        self.objects.append(Square(ev.x-5,ev.y-5,10,10))
+        self.queue_draw_area(0,0,1000,1000)
+    def button_release_event(self, widget, ev):
+        print "button release event",ev.x,ev.y,ev.button
 
-    if width < height:
-        size = width
-    else:
-        size = height
+    def motion_event(self, widget, ev):
+        print "motion event",ev.x,ev.y
+    def expose_event(self, widget, event):
+        _, _, width, height = widget.allocation
 
-    pixmap = gtk.gdk.Pixmap (widget.window, width, height)
-    ctx = pixmap.cairo_create()
+        if width < height:
+            size = width
+        else:
+            size = height
 
-    # set the background
-    ctx.set_source_rgb(0.7,0.7,0.7)
-    ctx.set_operator (cairo.OPERATOR_SOURCE)
-    ctx.paint()
+        pixmap = gtk.gdk.Pixmap (widget.window, width, height)
+        ctx = pixmap.cairo_create()
 
-    #ctx.translate ((width - size) / 2, (height - size) / 2)
-    #ctx.scale(size / 150.0, size / 160.0)
-    print objects
-    for obj in objects:
-        print "draw obj",obj
-        obj.Draw(ctx)
+        # set the background
+        ctx.set_source_rgb(0.7,0.7,0.7)
+        ctx.set_operator (cairo.OPERATOR_SOURCE)
+        ctx.paint()
 
-    # draw on window
-    gc = gtk.gdk.GC(widget.window)
-    widget.window.draw_drawable(gc, pixmap, 0,0, 0,0, -1,-1)
+        #ctx.translate ((width - size) / 2, (height - size) / 2)
+        #ctx.scale(size / 150.0, size / 160.0)
+        for obj in self.objects:
+            obj.Draw(ctx)
 
+        # draw on window
+        gc = gtk.gdk.GC(widget.window)
+        widget.window.draw_drawable(gc, pixmap, 0,0, 0,0, -1,-1)
 
-win = gtk.Window()
-win.add_events(gtk.gdk.KEY_PRESS_MASK |
-             gtk.gdk.POINTER_MOTION_MASK |
-             gtk.gdk.BUTTON_PRESS_MASK |
-             gtk.gdk.BUTTON_RELEASE_MASK |
-             gtk.gdk.SCROLL_MASK)
+ 
+def run(Widget,title="test app"):
+    window = gtk.Window()
+    window.set_title(title)
+    window.connect("delete-event", gtk.main_quit)
+    widget = Widget()
+    widget.show()
+    window.add(widget)
+    window.present()
+    gtk.main()
 
-win.connect('destroy', gtk.main_quit)
-win.connect('key_press_event', key_event)
-win.connect('button_press_event', button_press_event)
-win.connect('button_release_event', button_release_event)
-win.connect('motion_notify_event', motion_event)
-win.set_title('pygraph test')
-
-drawingarea = gtk.DrawingArea()
-win.add(drawingarea)
-drawingarea.connect('expose_event', expose_event)
-drawingarea.set_size_request(300,320)
-win.show_all()
-gtk.main()
+run(GtkBackend,"pygraph test")
 
