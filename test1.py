bbef0362050eed097b4982f0c45edd2527396b2f
curro@otredad <currojerez@gmail.com>
2008-03-20 21:07:47 +0100

    Arrow

--- test1.py
+++ test1.py
@@ -24,11 +24,35 @@
 #
 # Author: Kevin Worth <kevin@theworths.org>
 
-from math import pi
+from math import pi,atan2
 from random import randint
 
 import cairo
 import gtk
+
+
+class Arrow(object):
+	def __init__(self,color,x0,y0,x1,y1):
+		self.x0=x0
+		self.x1=x1
+		self.y0=y0
+		self.y1=y1
+		self.color=color
+	def Draw(self,ctx):
+		ctx.set_line_width(1)
+		ctx.set_source_rgb(*self.color)
+		ctx.move_to(self.x0,self.y0)
+		ctx.line_to(self.x1,self.y1)
+		angle=atan2((self.y1-self.y0),(self.x1-self.x0))
+		ctx.rotate(angle)
+		ctx.rel_line_to(-6,0)
+		ctx.rel_line_to(0,3)
+		ctx.rel_line_to(6,-3)
+		ctx.rel_line_to(-6,-3)
+		ctx.rel_line_to(0,3)
+		ctx.fill_preserve()
+		ctx.stroke()
+		
 
 #print "The secret word is " + word_chosen
 
@@ -37,12 +61,18 @@ def key_event(widget, event):
 
 def button_event(widget, event):
 	print "button event"
+	print event.x,event.y
+	_, _, width, height = widget.allocation
+
+	ctx.append_path(p)
+	print ctx.in_fill(event.x,event.y)
 
 def motion_event(widget, event):
 	print "motion event"
 
 def expose_event(widget, event):
     _, _, width, height = widget.allocation
+    global p,ctx
 
     if width < height:
         size = width
@@ -53,9 +83,23 @@ def expose_event(widget, event):
     ctx = pixmap.cairo_create()
 
     # set the background
-    ctx.set_source_rgb(0.7,0.7,0.7)
+    ctx.set_source_rgb(0.9,0.7,0.7)
     ctx.set_operator (cairo.OPERATOR_SOURCE)
+
     ctx.paint()
+
+    ctx.set_source_rgb(0,0,0)
+    ctx.set_line_width(7)
+    ctx.rectangle(10,10,100,100)
+    p=ctx.copy_path()
+    ctx.stroke()
+
+    
+    Arrow((0,0,1),100,100,200.0,300.0).Draw(ctx)
+
+    
+    ctx.append_path(p)
+
 
     ctx.translate ((width - size) / 2, (height - size) / 2)
     ctx.scale(size / 150.0, size / 160.0)
@@ -81,6 +125,7 @@ drawingarea = gtk.DrawingArea()
 win.add(drawingarea)
 drawingarea.connect('expose_event', expose_event)
 drawingarea.set_size_request(300,320)
+
 
 win.show_all()
 gtk.main()
