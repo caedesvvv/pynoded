6c1e2b84b629825150d9ba3e964317658a984bd2
Pablo Martin <caedes@delirium.(none)>
2008-03-20 19:43:49 +0100

    added scale control.

--- test1.py
+++ test1.py
@@ -32,13 +32,40 @@ class GtkBackend(gtk.DrawingArea):
         self.connect('button_release_event', self.button_release_event)
         self.connect('motion_notify_event', self.motion_event)
         self.connect('expose_event', self.expose_event)
+        self.connect('scroll_event', self.scroll_event)
         self.objects = []
+        self.scale = (1.0,1.0)
+        self.press_cbs = [self.Create,self.StartScale]
+        self.release_cbs = [None,None]
     def key_event(self, widget, ev):
         print "key event"
+    def Create(self,x,y):
+        print "create"
+        self.objects.append(Square(x-5,y-5,10,10))
+        return True
+    def StartScale(self,x,y):
+        self.scale_start = (x,y)
+        self.connect('motion_notify_event', self.ScaleChange)
+    def ScaleChange(self, widget, ev):
+        scale_x = ev.x
+        scale_y = ev.y
+    def EndScale(self,x,y):
+        pass
+    def scroll_event(self, widget, ev):
+        print "scroll event",ev.x,ev.y,dir(ev),ev.direction
+        if ev.direction == gtk.gdk.SCROLL_DOWN:
+            factor = -0.01
+        else:
+            factor = 0.01
+        self.scale = map(lambda s: s+factor,self.scale)
+        self.queue_draw_area(0,0,1000,1000)
     def button_press_event(self, widget, ev):
         print "button press event",ev.x,ev.y,ev.button
-        self.objects.append(Square(ev.x-5,ev.y-5,10,10))
-        self.queue_draw_area(0,0,1000,1000)
+        x = (ev.x)/self.scale[0]
+        y = (ev.y)/self.scale[1]
+        callback = self.press_cbs[ev.button-1]
+        if callback and callback(x,y):
+            self.queue_draw_area(0,0,1000,1000)
     def button_release_event(self, widget, ev):
         print "button release event",ev.x,ev.y,ev.button
 
@@ -54,11 +81,14 @@ class GtkBackend(gtk.DrawingArea):
 
         pixmap = gtk.gdk.Pixmap (widget.window, width, height)
         ctx = pixmap.cairo_create()
-
+        
         # set the background
         ctx.set_source_rgb(0.7,0.7,0.7)
         ctx.set_operator (cairo.OPERATOR_SOURCE)
         ctx.paint()
+
+        #ctx.translate ((width - size) / 2, (height - size) / 2)
+        ctx.scale(*self.scale)
 
         #ctx.translate ((width - size) / 2, (height - size) / 2)
         #ctx.scale(size / 150.0, size / 160.0)
@@ -68,7 +98,6 @@ class GtkBackend(gtk.DrawingArea):
         # draw on window
         gc = gtk.gdk.GC(widget.window)
         widget.window.draw_drawable(gc, pixmap, 0,0, 0,0, -1,-1)
-
  
 def run(Widget,title="test app"):
     window = gtk.Window()
