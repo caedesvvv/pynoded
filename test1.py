2d9f78983dc25a07990928da5c15cac9b014917d
Pablo Martin <caedes@delirium.(none)>
2008-03-20 19:02:21 +0100

    removed copyright notice from original file, of which remains, basically, nothing.

--- test1.py
+++ test1.py
@@ -1,37 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2004 Kevin Worth
-#
-# Permission to use, copy, modify, distribute, and sell this software
-# and its documentation for any purpose is hereby granted without fee,
-# provided that the above copyright notice appear in all copies and
-# that both that copyright notice and this permission notice appear in
-# supporting documentation, and that the name of the Kevin Worth not
-# be used in advertising or publicity pertaining to distribution of
-# the software without specific, written prior permission. Kevin Worth
-# California makes no representations about the suitability of this
-# software for any purpose.  It is provided "as is" without express or
-# implied warranty.
-#
-# KEVIN WORTH DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
-# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
-# NO EVENT SHALL KEVIN WORTH BE LIABLE FOR ANY SPECIAL, INDIRECT OR
-# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
-# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
-# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
-# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-#
-# Author: Kevin Worth <kevin@theworths.org>
-
-from math import pi
-from random import randint
-
 import cairo
 import gtk
-
-
-#print "The secret word is " + word_chosen
 
 class Square(object):
     def __init__(self,x,y,w,h,col=(0.1,0.1,0.1)):
