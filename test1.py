c43a788aad966952c327f8c473afc77a5288d2a1
curro@otredad <currojerez@gmail.com>
2008-03-20 22:25:35 +0100

    GraphObject class added Dictionary to map keyboard events to functions

--- test1.py
+++ test1.py
@@ -7,39 +7,39 @@ import cairo
 import gtk
 
 class GraphObject(object):
-	def __init__(self):
-		pass
-	def press(self,x,y):
-		pass
-	def release(self,x,y):
-		pass
-	def move(self,x,y):
-		pass
-	def Draw(self,ctx,scale):
-		pass
+    def __init__(self):
+        pass
+    def press(self,x,y):
+        pass
+    def release(self,x,y):
+        pass
+    def move(self,x,y):
+        pass
+    def Draw(self,ctx,scale):
+        pass
 
 class Arrow(object):
-	def __init__(self,color,x0,y0,x1,y1):
-		self.x0=x0
-		self.x1=x1
-		self.y0=y0
-		self.y1=y1
-		self.color=color
+    def __init__(self,color,x0,y0,x1,y1):
+        self.x0=x0
+        self.x1=x1
+        self.y0=y0
+        self.y1=y1
+        self.color=color
 	def Draw(self,ctx):
-		ctx.set_line_width(1)
-		ctx.set_source_rgb(*self.color)
-		ctx.move_to(self.x0,self.y0)
-		ctx.line_to(self.x1,self.y1)
-		angle=atan2((self.y1-self.y0),(self.x1-self.x0))
-		ctx.rotate(angle)
-		ctx.rel_line_to(-6,0)
-		ctx.rel_line_to(0,3)
-		ctx.rel_line_to(6,-3)
-		ctx.rel_line_to(-6,-3)
-		ctx.rel_line_to(0,3)
-		ctx.fill_preserve()
-		ctx.stroke()
-		
+            ctx.set_line_width(1)
+            ctx.set_source_rgb(*self.color)
+            ctx.move_to(self.x0,self.y0)
+            ctx.line_to(self.x1,self.y1)
+            angle=atan2((self.y1-self.y0),(self.x1-self.x0))
+            ctx.rotate(angle)
+            ctx.rel_line_to(-6,0)
+            ctx.rel_line_to(0,3)
+            ctx.rel_line_to(6,-3)
+            ctx.rel_line_to(-6,-3)
+            ctx.rel_line_to(0,3)
+            ctx.fill_preserve()
+            ctx.stroke()
+            
 
 class Square(object):
     def __init__(self,x,y,w,h,col=(0.1,0.1,0.1)):
@@ -60,11 +60,11 @@ class GtkBackend(gtk.DrawingArea):
     def __init__(self):
         super(GtkBackend,self).__init__()
         self.add_events(gtk.gdk.KEY_PRESS_MASK |
-                     gtk.gdk.POINTER_MOTION_MASK |
-                     gtk.gdk.BUTTON_PRESS_MASK |
-                     gtk.gdk.BUTTON_RELEASE_MASK |
-                     gtk.gdk.SCROLL_MASK)
-        self.connect('key_press_event', self.key_event)
+                        gtk.gdk.POINTER_MOTION_MASK |
+                        gtk.gdk.BUTTON_PRESS_MASK |
+                        gtk.gdk.BUTTON_RELEASE_MASK |
+                        gtk.gdk.SCROLL_MASK)
+        self.connect('key_press_event', self.key_press_event)
         self.connect('button_press_event', self.button_press_event)
         self.connect('button_release_event', self.button_release_event)
         self.connect('motion_notify_event', self.motion_event)
@@ -72,16 +72,27 @@ class GtkBackend(gtk.DrawingArea):
         self.connect('scroll_event', self.scroll_event)
         self.objects = []
         self.scale = (1.0,1.0)
-        self.press_cbs = [self.Create,None,self.StartMove]
-        self.release_cbs = [None,None,self.EndMove]
+	self.keyp_cbs = {"+" : lambda ev : Zoom(ev.x,ev.y,1/0.99),
+                         "-" : lambda ev : Zoom(ev.x,ev.y,0.99)}
+	self.keyr_cbs = {}
+        self.mousep_cbs = [self.Create,None,self.StartMove]
+        self.mouser_cbs = [None,None,self.EndMove]
         self.pos = (0.0,0.0)
-    def key_event(self, widget, ev):
-        print "key event"
+
+    def key_press_event(self, widget, ev):
+        print "KEY EVENT"
+        keyp_cbs[ev.string](ev.x,ev.y)
+
+    def key_release_event(self,widget, ev):
+        keyr_cbs[ev.string](ev.x,ev.y)
+
     def StartMove(self,x,y):
         self.movepos = [x,y]
         self.connect('motion_notify_event', self.move_screen)
+
     def EndMove(self,x,y):
         self.disconnect_by_func(self.move_screen)
+
     def move_screen(self,widget,ev):
         newpos = self.Screen2Surface(ev.x,ev.y)
         x = newpos[0]-self.movepos[0]
@@ -99,25 +110,27 @@ class GtkBackend(gtk.DrawingArea):
         x = float(x)
         y = float(y)
         return [(x/self.scale[0])-self.pos[0],(y/self.scale[1])-self.pos[1]]
-    def scroll_event(self, widget, ev):
+    def Zoom(self,x,y,factor):
         pre_pos = self.Screen2Surface(ev.x,ev.y)
-        if ev.direction == gtk.gdk.SCROLL_DOWN:
-            factor = 0.99
-        else:
-            factor = 1.01
         self.scale = map(lambda s: s*factor,self.scale)
         post_pos = self.Screen2Surface(ev.x,ev.y)
         self.pos = map(lambda i: self.pos[i]+post_pos[i]-pre_pos[i],range(2))
         self.Redraw()
+
+    def scroll_event(self, widget, ev):
+        if ev.direction == gtk.gdk.SCROLL_DOWN:
+            Zoom(ev.x,ev.y,0.99)
+        else:
+            Zoom(ev.x,ev.y,1/0.99)
     def button_press_event(self, widget, ev):
         x,y = self.Screen2Surface(ev.x,ev.y)
-        callback = self.press_cbs[ev.button-1]
+        callback = self.mousep_cbs[ev.button-1]
         if callback and callback(x,y):
             self.Redraw()
     def button_release_event(self, widget, ev):
         print "button release event",ev.x,ev.y,ev.button
         x,y = self.Screen2Surface(ev.x,ev.y)
-        callback = self.release_cbs[ev.button-1]
+        callback = self.mouser_cbs[ev.button-1]
         if callback and callback(x,y):
             self.Redraw()
     def motion_event(self, widget, ev):
@@ -149,7 +162,7 @@ class GtkBackend(gtk.DrawingArea):
         # draw on window
         gc = gtk.gdk.GC(widget.window)
         widget.window.draw_drawable(gc, pixmap, 0,0, 0,0, -1,-1)
- 
+        
 def run(Widget,title="test app"):
     window = gtk.Window()
     window.set_title(title)
