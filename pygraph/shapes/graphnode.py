64eebb889dd663c4987360a0c3b84fa8ba9f8d46
Pablo Martin <caedes@delirium.(none)>
2008-03-21 01:28:41 +0100

    made graphnode support variable number of inputs and outputs.

--- pygraph/shapes/graphnode.py
+++ pygraph/shapes/graphnode.py
@@ -3,15 +3,29 @@
 #
 
 from pygraph.shapes import Square,Circle
+import random
+
+colors = [(1,0,0),(0,1,0),(1,1,1),(1,0,1),(0,0,1)]
 
 class GraphNode(Square):
     def __init__(self,x,y,w,h):
         Square.__init__(self,x,y,w,h)
-        inp_r = h/11.
-        stride = h/5
-        self.stride = stride
-        self.inputs = [Circle(0,0,inp_r,inp_r),Circle(0,0,inp_r,inp_r)]
-        self.outputs = [Circle(0,0,inp_r,inp_r),Circle(0,0,inp_r,inp_r)]
+        self.inp_r = h/15.
+        self.stride = h/5
+        self.inputs = []
+        self.outputs = []
+        ninlets = random.randint(0,4)
+        noutlets = random.randint(0,4)
+        for i in xrange(ninlets):
+            col_idx = random.randint(0,4)
+            self.AddInlet(colors[col_idx])
+        for i in xrange(noutlets):
+            col_idx = random.randint(0,4)
+            self.AddOutlet(colors[col_idx])
+    def AddInlet(self,col):
+        self.inputs.append(Circle(0,0,self.inp_r,self.inp_r,col))
+    def AddOutlet(self,col):
+        self.outputs.append(Circle(0,0,self.inp_r,self.inp_r,col))
     def Draw(self,ctx):
         Square.Draw(self,ctx)
         ctx.save()
