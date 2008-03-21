c1bbd22739e9b7805529edfd008173829ec067e7
Pablo Martin <caedes@delirium.(none)>
2008-03-21 01:07:29 +0100

    added use of state to handle drawing of children.

--- pygraph/shapes/graphnode.py
+++ pygraph/shapes/graphnode.py
@@ -9,12 +9,20 @@ class GraphNode(Square):
         Square.__init__(self,x,y,w,h)
         inp_r = h/11.
         stride = h/5
-        self.inputs = [Circle(x,y+stride,inp_r,inp_r),Circle(x,y+stride*2,inp_r,inp_r)]
-        self.outputs = [Circle(x+w,y+stride,inp_r,inp_r),Circle(x+w,y+stride*2,inp_r,inp_r)]
+        self.stride = stride
+        self.inputs = [Circle(0,0,inp_r,inp_r),Circle(0,0,inp_r,inp_r)]
+        self.outputs = [Circle(0,0,inp_r,inp_r),Circle(0,0,inp_r,inp_r)]
     def Draw(self,ctx):
         Square.Draw(self,ctx)
+        ctx.save()
+        ctx.translate(self.x,self.y)
         for i,input in enumerate(self.inputs):
-            #ctx.move_to (self.x, self.y+6*i)
+            ctx.translate(0,self.stride)
             input.Draw(ctx)
+        ctx.restore()
+        ctx.save()
+        ctx.translate(self.x+self.w,self.y)
         for output in self.outputs:
+            ctx.translate(0,self.stride)
             output.Draw(ctx)
+        ctx.restore()
