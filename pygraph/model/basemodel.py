5dbaf4f46ef4e7e7cbac07078b8083d87f7cd994
Pablo Martin <caedes@delirium.(none)>
2008-12-04 07:33:43 +0100

    adding class properties to know which model classes can create which others.

--- pygraph/model/basemodel.py
+++ pygraph/model/basemodel.py
@@ -5,6 +5,7 @@ from meta import SubscribableModelMeta,ClassRegistry
 # reference model
 class SubscribableModel(object):
     __metaclass__ = SubscribableModelMeta
+
     def __init__(self,instance_uuid=None):
         self._evhs = []
         self._children = []
@@ -13,38 +14,52 @@ class SubscribableModel(object):
             self.uuid = uuid.UUID(instance_uuid)
         else:
             self.uuid = uuid.uuid1()
+
     def copyfrom(self,other):
         self._evhs = []
         self._children = other._children
         self.uuid = other.uuid
         self.invalidate()
+
     def new(self, clsname,*args,**kwargs):
         return ClassRegistry[clsname](*args,**kwargs)
+
     def subscribe(self, evh):
         if evh not in self._evhs:
             self._evhs.append(evh)
+
     def unsubscribe(self, evh):
         if evh in self._evhs:
             self._evhs.remove(evh)
+
     def post(self, name, *args):
         for evh in self._evhs:
             if hasattr(evh,name):
                 getattr(evh,name)(*args)
+
     def setProperty(self, name, value):
         self._props[name] = value
         self.post("propertychange",name,value)
+
     def getProperty(self, name):
         return self._props[name]
+
     def getProperties(self):
         return self._props.keys()
+
     def addChild(self, child):
         self._children.append(child)
         self.post("addchild",child)
+
     def delChild(self, child):
         self._children.remove(child)
         self.post("delchild",child)
+
     def getChildren(self):
         return self._children
+
     def invalidate(self):
         self.post("invalidate")
 
+    builds = []
+    builtby = []
